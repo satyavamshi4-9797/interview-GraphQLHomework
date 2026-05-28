38d8aa2979cf940f71c9473b88253cc0035bf472
satyavamshi4 <satyavamshi4@email.com>
2026-05-28 11:17:15 -0500

    fix metadata requestId included in error responses

--- src/server.ts
+++ src/server.ts
@@ -9,22 +9,7 @@ const yogaPort = 4000;
   const schema = await genSchema();
   const yoga = createYoga({
     schema,
-    plugins: [
-      // Ticket 3: enforce required 'client' header at the HTTP level
-      {
-        onRequest({ request, fetchAPI, endResponse }: any) {
-          const client = request.headers.get('client');
-          if (!client) {
-            const response = new fetchAPI.Response(
-              JSON.stringify({ errors: [{ message: 'Missing required header: client' }] }),
-              { status: 400, headers: { 'Content-Type': 'application/json' } }
-            );
-            endResponse(response);
-          }
-        },
-      },
-      ...plugins,
-    ],
+    plugins: [...plugins],
   });
   const server = createServer(yoga);
 
